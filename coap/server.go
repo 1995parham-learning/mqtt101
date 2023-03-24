b016163a5728ce5e299487a89461677dd69f35c0
Parham Alvani <parham.alvani@gmail.com>
2023-03-24 17:16:08 +0330

    coap sample code

---
+++ coap/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	coap "github.com/go-ocf/go-coap"
+	"github.com/go-ocf/go-coap/codes"
+)
+
+func handleA(w coap.ResponseWriter, req *coap.Request)  {
+	log.Printf("Got message in handleA: path=%q: %#v from %v", req.Msg.Path(), req.Msg, req.Client.RemoteAddr())
+	w.SetContentFormat(coap.TextPlain)
+	log.Println("Transmitting from A")
+	ctx, cancel := context.WithTimeout(req.Ctx, time.Second)
+	defer cancel()
+	if _, err := w.WriteWithContext(ctx, []byte("hello world")); err != nil {
+		log.Printf("Cannot send response: %v", err)
+	}
+}
+
+func handleB(w coap.ResponseWriter, req *coap.Request)  {
+	log.Printf("Got message in handleB: path=%q: %#v from %v", req.Msg.Path(), req.Msg, req.Client.RemoteAddr())
+	resp := w.NewResponse(codes.Content)
+	resp.SetOption(coap.ContentFormat, coap.TextPlain)
+	resp.SetPayload([]byte("good bye!"))
+	log.Printf("Transmitting from B %#v", resp)
+	ctx, cancel := context.WithTimeout(req.Ctx, time.Second)
+	defer cancel()
+	if _, err := w.WriteWithContext(ctx, []byte("hello world")); err != nil {
+		log.Printf("Cannot send response: %v", err)
+	}
+}
+
+func main() {
+	mux := coap.NewServeMux()
+	mux.Handle("/a", coap.HandlerFunc(handleA))
+	mux.Handle("/b", coap.HandlerFunc(handleB))
+
+	log.Fatal(coap.ListenAndServe("udp", ":1373", mux))
+}
