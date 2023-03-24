f66539a5de52e3534213c278d44ef950a7302869
Parham Alvani <parham.alvani@gmail.com>
2023-03-24 17:15:54 +0330

    Added OnConnectHandler

--- client/client.go
+++ client/client.go
@@ -1,14 +1,20 @@
 package client
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"sync"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
 type Client struct {
-	C MQTT.Client
+	c         MQTT.Client
+	opts      *MQTT.ClientOptions
+	topics    []string
+	mutex     sync.Mutex
+	connected bool
 }
 
 func New(server string, id string) Client {
@@ -26,29 +32,46 @@ func NewWithMessageHandler(server string, id string, f MQTT.MessageHandler) Clie
 	opts.SetClientID(id)
 	opts.SetDefaultPublishHandler(f)
 
-	cli := Client{C: MQTT.NewClient(opts)}
+	cli := Client{
+		opts:      opts,
+		connected: false,
+	}
 
 	return cli
 }
 
-func (cli *Client) Connect(topics ...string) {
-	if token := cli.C.Connect(); token.Wait() && token.Error() != nil {
+func (cli *Client) Connect() {
+	cli.connected = true
+
+	cli.opts.SetOnConnectHandler(func(client MQTT.Client) {
+		for _, topic := range cli.topics {
+			if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+				fmt.Println(token.Error())
+				os.Exit(1)
+			}
+		}
+	})
+
+	cli.c = MQTT.NewClient(cli.opts)
+
+	if token := cli.c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
-	}
-
-	for _, topic := range topics {
-		cli.Subscribe(topic)
-	}
-}
-
-func (cli *Client) Subscribe(topic string) {
-	if token := cli.C.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
 	}
 }
 
 func (cli *Client) Publish(topic string, text string) {
-	token := cli.C.Publish(topic, 0, false, text)
+	token := cli.c.Publish(topic, 0, false, text)
 	token.Wait()
+}
+
+func (cli *Client) Register(topics []string) error {
+	if cli.connected {
+		return errors.New("you can not register a new topic after connecting the client to the broker")
+	}
+
+	cli.mutex.Lock()
+	cli.topics = append(cli.topics, topics...)
+	cli.mutex.Unlock()
+
+	return nil
 }
